fa9e9d2b0d92624982c9d409de174f5c21086482
Ely Johnson <elyjohnson6@gmail.com>
2024-01-25 15:20:50 -0600

    Wrote tests for List all the todos and for several future implementations
    Co-Authored-by Ty Beasley <beasl110@morris.umn.edu>

---
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -0,0 +1,211 @@
+package umm3601.todo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import io.javalin.Javalin;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import io.javalin.http.NotFoundResponse;
+import umm3601.Main;
+import umm3601.user.UserController;
+import umm3601.user.UserDatabase;
+
+/**
+ * Tests the logic of the TodoController
+ *
+ * @throws IOException
+ */
+
+@SuppressWarnings({"MagicNumber"})
+public class TodoControllerSpec {
+  private TodoController todoController;
+  private static TodoDatabase db;
+
+  @Mock
+  private Context ctx;
+
+  @Captor
+  private ArgumentCaptor<Todo[]> todoArrayCaptor;
+
+   /**
+   * Setup the "database" with some example users and
+   * create a UserController to exercise in the tests.
+   *
+   * @throws IOException if there are problems reading from the "database" file.
+   */
+
+  @BeforeEach
+  public void setUp() throws IOException {
+    MockitoAnnotations.openMocks(this);
+    db = new TodoDatabase(Main.TODO_DATA_FILE);
+    todoController = new TodoController(db);
+  }
+
+  @Test
+  public void canBuildController() throws IOException {
+    // Call the `TodoController.buildTodoController` method
+    // to construct a controller instance "by hand".
+    TodoController controller = TodoController.buildTodoController(Main.TODO_DATA_FILE);
+    Javalin mockServer = Mockito.mock(Javalin.class);
+    controller.addRoutes(mockServer);
+
+    // Verify that calling `addRoutes()` above caused `get()` to be called
+    // on the server at least twice. We use `any()` to say we don't care about
+    // the arguments that were passed to `.get()`.
+    verify(mockServer, Mockito.atLeast(2)).get(any(), any());
+  }
+
+  @Test
+  public void buildControllerFailsWithIllegalDbFile() {
+    Assertions.assertThrows(IOException.class, () -> {
+      TodoController.buildTodoController("this is not a legal file name");
+    });
+  }
+
+  @Test
+  public void canGetAllUsers() throws IOException {
+    todoController.getTodos(ctx);
+    verify(ctx).json(todoArrayCaptor.capture());
+    assertEquals(db.size(), todoArrayCaptor.getValue().length);
+  }
+
+  /**
+   * Confirm that we can get all the users with owner Fry.
+   *
+   * @throws IOException if there are problems reading from the "database" file.
+   */
+  @Test
+  public void canGetUsersWithOwnerFry() throws IOException {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("owner", Arrays.asList(new String[] {"Fry"}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+
+    todoController.getTodos(ctx);
+
+    // Confirm that all the todos passed to `json` have owner Fry.
+    verify(ctx).json(todoArrayCaptor.capture());
+    for (Todo todo : todoArrayCaptor.getValue()) {
+      assertEquals("Fry", todo.owner);
+    }
+  }
+
+  /**
+   * Confirm that we can get all the todos with category video games.
+   *
+   * @throws IOException if there are problems reading from the "database" file.
+   */
+  @Test
+  public void canGetUsersWithCategoryVideoGames() throws IOException {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("category", Arrays.asList(new String[] {"video games"}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+
+    todoController.getTodos(ctx);
+
+    // Confirm that all the todos passed to `json` have owner Fry.
+    verify(ctx).json(todoArrayCaptor.capture());
+    for (Todo todo : todoArrayCaptor.getValue()) {
+      assertEquals("video games", todo.owner);
+    }
+  }
+
+  /**
+   * Confirm that we can get all the todos with a body that contains sit.
+   *
+   * @throws IOException if there are problems reading from the "database" file.
+   */
+  @Test
+  public void canGetUsersWithBodyContainsSit() throws IOException {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("body", Arrays.asList(new String[] {"Fry"}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+
+    todoController.getTodos(ctx);
+
+    // Confirm that all the todos passed to `json` contain sit in body.
+    verify(ctx).json(todoArrayCaptor.capture());
+    for (Todo todo : todoArrayCaptor.getValue()) {
+      assertEquals("Fry", todo.owner);
+    }
+  }
+
+  /**
+   * Confirm that we can get all the users with owner Fry, category video games, and status true.
+   * This is a "combination" test that tests the interaction of the
+   * `owner` and `category`, and 'status' query parameters.
+   *
+   * @throws IOException if there are problems reading from the "database" file.
+   */
+  @Test
+  public void canGetUsersWithGivenOwnerAndCategoryAndStatus() throws IOException {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("category", Arrays.asList(new String[] {"video games"}));
+    queryParams.put("owner", Arrays.asList(new String[] {"Fry"}));
+    queryParams.put("status", Arrays.asList(new String[] {"true"}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+
+    todoController.getTodos(ctx);
+
+    // Confirm that all the users passed to `json` have owner Fry, category video games, and status true
+    verify(ctx).json(todoArrayCaptor.capture());
+    for (Todo todo : todoArrayCaptor.getValue()) {
+      assertEquals("Fry", todo.owner);
+      assertEquals("video games", todo.category);
+      assertEquals(true, todo.status);
+    }
+  }
+
+  /**
+   * Confirm that we get a todo when using a valid user ID.
+   *
+   * @throws IOException if there are problems reading from the "database" file.
+   */
+  @Test
+  public void canGetUserWithSpecifiedId() throws IOException {
+    // A specific todo ID known to be in the "database".
+    String id = "588935f5c668650dc77df581";
+    // Get the todo associated with that ID.
+    Todo todo = db.getTodo(id);
+
+    when(ctx.pathParam("id")).thenReturn(id);
+
+    todoController.getTodo(ctx);
+
+    verify(ctx).json(todo);
+    verify(ctx).status(HttpStatus.OK);
+  }
+
+  /**
+   * Confirm that we get a 404 Not Found response when
+   * we request a todo ID that doesn't exist.
+   *
+   * @throws IOException if there are problems reading from the "database" file.
+   */
+  @Test
+  public void respondsAppropriatelyToRequestForNonexistentId() throws IOException {
+    when(ctx.pathParam("id")).thenReturn(null);
+    Throwable exception = Assertions.assertThrows(NotFoundResponse.class, () -> {
+      todoController.getTodo(ctx);
+    });
+    assertEquals("No user with id " + null + " was found.", exception.getMessage());
+  }
+}
