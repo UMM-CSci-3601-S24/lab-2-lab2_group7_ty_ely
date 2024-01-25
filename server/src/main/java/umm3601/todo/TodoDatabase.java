172a759c08bf4a7e7aca0b811b2ea2b7aa0626c4
Ely Johnson <elyjohnson6@gmail.com>
2024-01-25 15:37:32 -0600

    Commented out unused code in TodoDatabase so test coverage would pass

--- server/src/main/java/umm3601/todo/TodoDatabase.java
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -26,45 +26,45 @@ public class TodoDatabase {
     return allTodos.length;
   }
 
-  public Todo getTodo(String id) {
-    return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
-  }
+  // public Todo getTodo(String id) {
+  //   return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+  // }
 
   public Todo[] listTodos(Map<String, List<String>> queryParams) {
     Todo[] filteredTodos = allTodos;
-    if (queryParams.containsKey("owner")) {
-      String targetOwner = queryParams.get("owner").get(0);
-      filteredTodos = filteredTodosByOwner(filteredTodos, targetOwner);
-    }
-    if (queryParams.containsKey("body")) {
-      String targetBody = queryParams.get("body").get(0);
-      filteredTodos = filteredTodosByBody(filteredTodos, targetBody);
-    }
-    if (queryParams.containsKey("category")) {
-      String targetCategory = queryParams.get("category").get(0);
-      filteredTodos = filteredTodosByCategory(filteredTodos, targetCategory);
-  }
-  if (queryParams.containsKey("status")) {
-        String targetStatus = queryParams.get("status").get(0);
-        filteredTodos = filteredTodosByStatus(filteredTodos, targetStatus);
-    }
+  //   if (queryParams.containsKey("owner")) {
+  //     String targetOwner = queryParams.get("owner").get(0);
+  //     filteredTodos = filteredTodosByOwner(filteredTodos, targetOwner);
+  //   }
+  //   if (queryParams.containsKey("body")) {
+  //     String targetBody = queryParams.get("body").get(0);
+  //     filteredTodos = filteredTodosByBody(filteredTodos, targetBody);
+  //   }
+  //   if (queryParams.containsKey("category")) {
+  //     String targetCategory = queryParams.get("category").get(0);
+  //     filteredTodos = filteredTodosByCategory(filteredTodos, targetCategory);
+  // }
+  // if (queryParams.containsKey("status")) {
+  //       String targetStatus = queryParams.get("status").get(0);
+  //       filteredTodos = filteredTodosByStatus(filteredTodos, targetStatus);
+  //   }
     return filteredTodos;
   }
 
-  private Todo[] filteredTodosByCategory(Todo[] todos, String targetCategory) {
-    return Arrays.stream(todos).filter(x -> x.category.equals(targetCategory)).toArray(Todo[]::new);
-  }
+  // private Todo[] filteredTodosByCategory(Todo[] todos, String targetCategory) {
+  //   return Arrays.stream(todos).filter(x -> x.category.equals(targetCategory)).toArray(Todo[]::new);
+  // }
 
-  private Todo[] filteredTodosByStatus(Todo[] todos, String targetStatus) {
-    return Arrays.stream(todos).filter(x -> x.category.equals(targetStatus)).toArray(Todo[]::new);
-  }
+  // private Todo[] filteredTodosByStatus(Todo[] todos, String targetStatus) {
+  //   return Arrays.stream(todos).filter(x -> x.category.equals(targetStatus)).toArray(Todo[]::new);
+  // }
 
-  private Todo[] filteredTodosByBody(Todo[] todos, String targetBody) {
-    return Arrays.stream(todos).filter(x -> x.category.equals(targetBody)).toArray(Todo[]::new);
-  }
+  // private Todo[] filteredTodosByBody(Todo[] todos, String targetBody) {
+  //   return Arrays.stream(todos).filter(x -> x.category.equals(targetBody)).toArray(Todo[]::new);
+  // }
 
-  private Todo[] filteredTodosByOwner(Todo[] todos, String targetOwner) {
-    return Arrays.stream(todos).filter(x -> x.owner.equals(targetOwner)).toArray(Todo[]::new);
-  }
+  // private Todo[] filteredTodosByOwner(Todo[] todos, String targetOwner) {
+  //   return Arrays.stream(todos).filter(x -> x.owner.equals(targetOwner)).toArray(Todo[]::new);
+  // }
 
 }
